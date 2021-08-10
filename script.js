94de36b4bcf30e24ed5eb19574a0c5e7f6cca227
CarlosArro2001 <carlosarrowork@gmail.com>
2021-08-10 18:01:16 +0300

    Implementation of bootstrap and new updates
    New updates include:
    1. Implementation of bootstrap for it's grid system.
    2. Able to toggle task list and it's form
    3. Timer options which are able to be toggled

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 /*
     Author : Carlos Raniel Ariate Arro 
 
-    Last Update Date & Time : Thursday,July 30 2021  10:45pm
+    Last Update Date & Time : Tuesday,August 10 2021  5:55pm
 
-    timer functions: tempalate() start()
-    task list functions : AddTask()
 */
 
 // preset values for the timer function template 
@@ -12,21 +10,30 @@ var min = 25;
 var sec = "00";
 var hrs ="00";
 
-// when the page is first loaded this function will be called to immediately 
-// put the timer on 25:00
 function template(){
     document.getElementById("hours").innerHTML = hrs;
     document.getElementById("minutes").innerHTML = min;
     document.getElementById("seconds").innerHTML = sec;
 }
+
+
+
+function showOptions(){
+    var options = document.getElementById("timer-options");
+    options.style.setProperty("display","block","important");
+}
+
+
+var minutes_interval = 0;
+var seconds_interval = 0;
 function start(){
-    min = 24;
+    min = min-1;
     sec = 59;
     document.getElementById("minutes").innerHTML= min;
     document.getElementById("seconds").innerHTML = sec;
     
-    var minutes_interval = setInterval(minutesTimer,60000);
-    var seconds_interval = setInterval(secondsTimer,1000);
+    minutes_interval = setInterval(minutesTimer,60000);
+    seconds_interval = setInterval(secondsTimer,1000);
 
     function minutesTimer(){
         min-min-1;
@@ -44,57 +51,90 @@ function start(){
         }
     }
 }
-var check_no = 0;
-// function will be invoked after submit button located in the Task list form clicked
-function AddTask(){
-    var task = document.createElement('div');
-    task.className="task";
-    var li = document.createElement('li');
-    li.className = 'li';
-    var box = document.createElement('label');
-    box.className = "box";
-    var input = document.querySelector('#TaskInput').value;
-    var text = document.createElement("label");
-    text.innerHTML = input;
-    //generating check element
-    /* Need to figure out how to dynamically create checkboxes properly */
-    var checkbox = document.createElement("input");
-    checkbox.type="checkbox";
-    checkbox.className="check";
-    check_no += 1
-    checkbox.id="check " +check_no;
-    var checkmark = document.createElement('label');
-    checkmark.htmlFor = checkbox.id;
-    checkmark.className = "checkmark";
-    //chkbutton.onclick = tickFunc();
-    //delete button
-    var btnRMV = document.createElement("span");
-    btnRMV.setAttribute("class","far fa-trash-alt");
-    //calling the removeTask() function when the button is clicked 
-    var remove = document.createElement("button");
-    remove.onclick= function(){
-        this.parentNode.parentNode.remove();
-    }
-    //adding the elements
-    li.appendChild(task);
-    task.appendChild(box);
-    task.appendChild(remove);
-    box.appendChild(checkbox);
-    box.appendChild(checkmark);
-    box.appendChild(text);
-    remove.appendChild(btnRMV);
-    // checking whether or not the user has entered a task or not
-    if(input ===""){
-        alert("You must write something to add!");
-    }else{
-        document.getElementById("list").appendChild(li);
-    }
-    document.getElementById("TaskInput").value=""; 
-}
 
-function hideForm(){
-    document.getElementById("form").style.visibility = 'hidden';
+function clear(){
+    console.log("executed");
 }
 
 
 
+
+$("#menu-toggle").click(function (e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
+
+$("#menu-untoggle").click(function(e){
+    e.preventDefault();
+    $("#wrapper").not(this).removeClass("toggled");
+});
+
+
+  function toggleForm(){
+      var form = document.getElementById("form");
+      form.style.setProperty("display","block","important");
+  }
+  function hideForm(){
+      var form = document.getElementById("form");
+      form.style.setProperty("display","none","important");
+  }
+
+
+  var check_no = 0;
+  // function will be invoked after submit button located in the Task list form clicked
+  function AddTask(){
+      var task = document.createElement('div');
+      task.className="task";
+      var li = document.createElement('li');
+      li.className = 'li';
+      var box = document.createElement('label');
+      box.className = "box";
+      var input = document.querySelector('#TaskInput').value;
+      var text = document.createElement("label");
+      text.className = "text-label";
+      text.innerHTML = input;
+      //generating check element
+      /* Need to figure out how to dynamically create checkboxes properly */
+      var checkbox = document.createElement("input");
+      checkbox.type="checkbox";
+      checkbox.className="check";
+      check_no += 1
+      checkbox.id="check " +check_no;
+      var checkmark = document.createElement('label');
+      checkmark.htmlFor = checkbox.id;
+      checkmark.className = "checkmark";
+      //delete button
+      var btnRMV = document.createElement("span");
+      btnRMV.setAttribute("class","far fa-trash-alt");
+      //calling the removeTask() function when the button is clicked 
+      var remove = document.createElement("button");
+      remove.onclick= function(){
+          this.parentNode.parentNode.remove();
+      }
+      //adding the elements
+      li.appendChild(task);
+      task.appendChild(box);
+      task.appendChild(remove);
+      box.appendChild(checkbox);
+      box.appendChild(checkmark);
+      box.appendChild(text);
+      remove.appendChild(btnRMV);
+      // checking whether or not the user has entered a task or not
+      if(input ===""){
+          alert("You must write something to add!");
+      }else{
+          document.getElementById("list").appendChild(li);
+      }
+      document.getElementById("TaskInput").value=""; 
+  }
+
+
+function hideArrow(){
+    var btn = document.getElementById("menu-toggle");
+    btn.style.setProperty("display","none","important");
+}
+
+function showArrow(){
+    var btn = document.getElementById("menu-toggle");
+    btn.style.setProperty("display","block","important");
+}
